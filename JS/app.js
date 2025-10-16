0b879f7dc603cc7362e91dc91799a4a1b74ccd70
Wichern94 <Mateusz.kabaja@gmail.com>
2025-10-16 21:29:39 +0200

    feature: abaut section JS

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,29 @@
 import { initHeader } from './header.js';
-
+// odpal tylko jeśli na stronie jest <header>
 window.addEventListener('DOMContentLoaded', () => {
-  // odpal tylko jeśli na stronie jest <header>
   if (document.querySelector('header')) {
     initHeader();
   }
 });
+//Sekcja Abaut
+const createRadialMoveHandler = (card) => {
+  const handler = (e) => {
+    radialMove(card, e);
+  };
+  return handler;
+};
+
+const abautSection = document.getElementById('abaut');
+abautSection.querySelectorAll('.card').forEach((card) => {
+  const handler = createRadialMoveHandler(card);
+  card.__radialMoveHandler = handler;
+
+  card.addEventListener('mousemove', handler);
+});
+
+function radialMove(card, e) {
+  //aktualizacja var-CSS
+  const rect = card.getBoundingClientRect();
+  card.style.setProperty('--x', `${e.clientX - rect.left}px`);
+  card.style.setProperty('--y', `${e.clientY - rect.left}px`);
+}
