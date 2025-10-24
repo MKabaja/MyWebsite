22fcbcc10e153a9cbb6a23e0455ec128a924f44a
Wichern94 <Mateusz.kabaja@gmail.com>
2025-10-25 01:23:50 +0200

    feature: smooth scrolling

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 import { initHeader } from './header.js';
 import { initVisibility } from './core/visibility.js';
 import { initModels3D } from './core/spline.controler.js';
+import { createObserver } from './observer-manager.js';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 AOS.init();
@@ -10,159 +11,38 @@ window.addEventListener('DOMContentLoaded', () => {
     initHeader();
     initVisibility();
     initModels3D();
+    setupScrollDown();
   }
 });
 
-// const observer = createObserver({
-//   threshold: 0.15,
-//   rootMargin: '0px 0px -10% 0px',
-// });
-// // =======================================================
-// // Sekcja ABAUT
-// // =======================================================
-// const abautSection = document.getElementById('abaut');
-// const items = abautSection.querySelectorAll('.card');
+function setupScrollDown() {
+  const scrollDown = document.getElementById('scroll-down');
 
-// if (abautSection) {
-//   const addHandlers = () => {
-//     items.forEach((card) => {
-//       if (card.__radialMoveHandler) return;
-//       const handler = (e) => radialMove(card, e);
-//       card.__radialMoveHandler = handler;
-//       card.addEventListener('mousemove', handler);
-//     });
-//   };
-//   const removeHandlers = () => {
-//     items.forEach((card) => {
-//       if (!card.__radialMoveHandler) return;
-//       card.removeEventListener('mousemove', card.__radialMoveHandler);
-//       card.__radialMoveHandler = null;
-//     });
-//   };
+  const sections = document.querySelectorAll('section');
+  const io = createObserver({ threshold: 0.25, rootMargin: '0px 0px -10% 0px' });
+  io.registerAll(sections, (el) => ({
+    attach() {
+      if (el.id === 'contact') {
+        scrollDown.classList.add('duration-200', 'rotate-180');
 
-//   // =======================================================
-//   // INTERSECTION OBSERVER I ZARZĄDZANIE PAMIĘCIĄ
-//   // =======================================================
-//   observer.register(abautSection, {
-//     attach() {
-//       addHandlers();
-//     },
-//     detach() {
-//       removeHandlers();
-//     },
-//   });
-// }
-
-// function radialMove(card, e) {
-//   //aktualizacja var-CSS
-//   const rect = card.getBoundingClientRect();
-//   card.style.setProperty('--x', `${e.clientX - rect.left}px`);
-//   card.style.setProperty('--y', `${e.clientY - rect.top}px`);
-// }
-
-// =======================================================
-// Auto Play na hover
-// =======================================================
-// const projectsSection = document.getElementById('projects');
-
-// if (projectsSection) {
-//   const videos = projectsSection.querySelectorAll('video');
-//   const addHandlers = () => {
-//     videos.forEach((vid) => {
-//       if (vid.__vidPlayHandler || vid.__vidPlayHandler2) return;
-
-//       const handlerEnter = () => playHandler(vid);
-//       const handlerLeave = () => stopHandler(vid);
-
-//       vid.__vidPlayHandler = handlerEnter;
-//       vid.__vidPlayHandler2 = handlerLeave;
-
-//       vid.addEventListener('mouseenter', handlerEnter);
-//       vid.addEventListener('mouseleave', handlerLeave);
-//     });
-//   };
-//   const removeHandlers = () => {
-//     videos.forEach((vid) => {
-//       if (!vid.__vidPlayHandler || !vid.__vidPlayHandler2) return;
-
-//       vid.removeEventListener('mouseenter', vid.__vidPlayHandler);
-//       vid.removeEventListener('mouseleave', vid.__vidPlayHandler2);
-
-//       vid.__vidPlayHandler = null;
-//       vid.__vidPlayHandler2 = null;
-//     });
-//   };
-
-//   observer.register(projectsSection, {
-//     attach() {
-//       addHandlers();
-//     },
-//     detach() {
-//       removeHandlers();
-//     },
-//   });
-// }
-
-// function playHandler(vid) {
-//   vid.muted = true;
-//   vid.playsInline = true;
-//   vid.play().catch(() => {});
-// }
-// function stopHandler(vid) {
-//   vid.pause();
-//   vid.currentTime = 0;
-// }
-
-// // =======================================================
-// //  Form wiadomosci
-// // =======================================================
-// const contactSection = document.getElementById('contact');
-// const form = contactSection?.querySelector('#contact-form');
-// if (contactSection && form) {
-//   const onSubmit = (e) => {
-//     e.preventDefault();
-//     toastHandler();
-//     form.reset();
-//   };
-//   const addHandler = () => {
-//     if (form.__submitHandlerAttached) return;
-//     form.addEventListener('submit', onSubmit);
-//     form.__submitHandlerAttached = true;
-//   };
-//   const removeHandler = () => {
-//     if (!form.__submitHandlerAttached) return;
-//     form.removeEventListener('submit', onSubmit);
-//     form.__submitHandlerAttached = false;
-//   };
-
-//   observer.register(contactSection, {
-//     attach() {
-//       addHandler();
-//     },
-//     detach() {
-//       removeHandler();
-//     },
-//   });
-// }
-// function toastHandler() {
-//   const msg = contactSection.querySelector('#contact-form__succes');
-
-//   showMsg(msg);
-
-//   setTimeout(() => hideMsg(msg), 3000);
-// }
-
-// function showMsg(msg) {
-//   msg.classList.remove('hidden', 'opacity-0');
-//   requestAnimationFrame(() => msg.classList.add('opacity-100'));
-// }
-// function hideMsg(msg) {
-//   const onEnd = (e) => {
-//     if (e.propertyName !== 'opacity') return;
-//     msg.removeEventListener('transitionend', onEnd);
-//     msg.classList.add('hidden');
-//   };
-//   msg.addEventListener('transitionend', onEnd, { once: true });
-//   msg.classList.remove('opacity-100');
-//   msg.classList.add('opacity-0');
-// }
+        scrollDown.removeEventListener('click', scrollToFooter);
+        scrollDown.addEventListener('click', scrollToHome);
+      } else {
+        scrollDown.classList.remove('duration-200', 'rotate-180');
+        scrollDown.removeEventListener('click', scrollToHome);
+        scrollDown.addEventListener('click', scrollToFooter);
+      }
+    },
+    detach() {},
+  }));
+}
+function scrollToHome() {
+  const section = document.getElementById('header');
+  if (!section) return;
+  section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+function scrollToFooter() {
+  const section = document.getElementById('footer');
+  if (!section) return;
+  section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
